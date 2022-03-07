31bb350d6c412b71376c50c5441a7853640c2413
DaryusOctavyan <dariuschifor@gmail.com>
2022-03-07 19:20:50 +0200

    eep

---
+++ other/meme004/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef int64_t i64;
+
+int main() {
+    i64 n;
+    cin >> n;
+    // declare and read a vector of n elements
+    
+
+}
