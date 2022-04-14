ab3baf690f457cf1850a24118de5a059174ec6f1
DaryusOctavyan <dariuschifor@gmail.com>
2022-04-14 21:54:00 +0300

    eep

---
+++ kattis/LTAI-2022-04-12/F/prva.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef int64_t i64;
+
+int main() { return 0; }
+
+/*
+Alexia --->> 1st
+Oanna --->> 2nd
+Alexandra --->> 3rd
+Antonio Carlos da Silva Corona --->> 4th
+*/
