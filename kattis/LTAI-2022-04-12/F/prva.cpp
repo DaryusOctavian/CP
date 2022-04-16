e5bc4e4598e2d3c1c9e14480296b6f58ded79c89
DaryusOctavian <dariuschifor@gmail.com>
2022-04-16 15:22:00 +0300

    eep

--- kattis/LTAI-2022-04-12/F/prva.cpp
+++ kattis/LTAI-2022-04-12/F/prva.cpp
@@ -4,7 +4,95 @@ using namespace std;
 
 typedef int64_t i64;
 
-int main() { return 0; }
+/*
+  i64 ans = 0;
+  vector<i64> ps(26, 0);
+  for (i64 i = 0; i < s.size(); i++) {
+    // if (s[i] >= 'a' && s[i] <= 'z') {
+    ps[s[i] - 'a']++;
+    // }
+  }
+
+  for (i64 i = 0; i < 26; i++) {
+    if (ps[i] > 0) {
+      ans++;
+    }
+  }
+
+  return ans;
+
+*/
+
+i64 counter(string s) {
+  i64 res = 0;
+  for (auto x : s) {
+    res += x - 'a';
+  }
+
+  return res;
+}
+
+int main() {
+  i64 a, b;
+  cin >> a >> b;
+  char mp[a][b];
+  for (i64 i = 0; i < a; i++) {
+    for (i64 j = 0; j < b; j++) {
+      cin >> mp[i][j];
+    }
+  }
+
+  string currLine, strRes;
+  i64 res = 1e18;
+  for (i64 i = 0; i < a; i++) {
+    for (i64 j = 0; j < b; j++) {
+      if (mp[i][j] == '#') {
+        if (currLine.size() > 1) {
+          if (counter(currLine) < res) {
+            res = counter(currLine);
+            strRes = currLine;
+          }
+        }
+        currLine.clear();
+      } else {
+        currLine += mp[i][j];
+      }
+    }
+    if (currLine.size() > 1) {
+      if (counter(currLine) < res) {
+        res = counter(currLine);
+        strRes = currLine;
+      }
+    }
+    currLine.clear();
+  }
+
+  for (i64 i = 0; i < b; i++) {
+    for (i64 j = 0; j < a; j++) {
+      if (mp[j][i] == '#') {
+        if (currLine.size() > 1) {
+          if (counter(currLine) < res) {
+            res = counter(currLine);
+            strRes = currLine;
+          }
+        }
+        currLine.clear();
+      } else {
+        currLine += mp[j][i];
+      }
+    }
+    if (currLine.size() > 1) {
+      if (counter(currLine) < res) {
+        res = counter(currLine);
+        strRes = currLine;
+      }
+    }
+    currLine.clear();
+  }
+
+  cout << strRes << endl;
+  return 0;
+}
 
 /*
 Alexia --->> 1st
